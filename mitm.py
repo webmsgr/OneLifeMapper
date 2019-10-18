823f345a926d8df8beaf6562cda74fb4acdf57b8
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-18 12:49:40 -0700

    Update mitm.py

--- mitm.py
+++ mitm.py
@@ -1,36 +1,17 @@
 # @todo add mitm proxy
 import select
 import socket
-import multiprocessing
+import multiprocessing as mp
 import threading
 
-class MapChunk: 
-    def __init__(self,data=[[]]):
-        self.data = data
-    def get(x,y):
-        return self.data[y][x]
-    def set(x,y,data):
-        self.data[y][x] = data
-    def setrow(y,data):
-        self.data[y] = data
-    def setcol(x,data):
-        for n,data in enumerate(data):
-            self.set(x,n,data)
-    def __iter__(self):
-        return iter(self.data)
 
-def main(): # receves parsed map data and does things
+def main(saddr,cadder,outpipe): # receves packets and sends them to the caller
     pass
 
-  
-def task_manager(saddr,caddr,outpipe): # creates threads and the parsing pool and sends parsed map data back to the main thread
-    pass
 
 def server_worker(saddr,caddr,outpipe): # communicates with server/client
     pass
 
-def map_worker(data): # parses map data
-    pass
 
 def Server(pipe,msocket): # copied from bot, communicates
     msocket.setblocking(0)

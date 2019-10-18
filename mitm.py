77c3793fa410ddf88e662dbe1025d9879c461e99
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-18 12:29:00 -0700

    Update mitm.py

--- mitm.py
+++ mitm.py
@@ -1,1 +1,35 @@
 # @todo add mitm proxy
+import select
+import socket
+import multiprocessing
+import threading
+
+
+
+def main(): # receves parsed map data and does things
+    pass
+
+  
+def task_manager(): # creates threads and the parsing pool and sends parsed map data back to the main thread
+    pass
+
+def server_worker(): # communicates with server/client
+    pass
+
+def map_worker(data): # parses map data
+    pass
+
+def Server(pipe,msocket): # copied from bot, communicates
+    msocket.setblocking(0)
+    while True:
+        try:
+            read,write,error  = select.select([msocket],[msocket],[msocket],60)
+            if msocket in read:
+                buf = msocket.recv(2048)
+                pipe.send(buf)
+            if pipe.poll() and msocket in write:
+                msocket.send(pipe.recv())
+             if msocket in error:
+                break
+        except:
+            break

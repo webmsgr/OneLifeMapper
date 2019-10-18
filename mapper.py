56cbfc5df8241cea01893df66c727f122eb6489a
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-18 12:50:20 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,1 +1,21 @@
 # @todo add map program
+import mitm
+import multiprocessing
+
+class MapChunk: 
+    def __init__(self,data=[[]]):
+        self.data = data
+    def get(x,y):
+        return self.data[y][x]
+    def set(x,y,data):
+        self.data[y][x] = data
+    def setrow(y,data):
+        self.data[y] = data
+    def setcol(x,data):
+        for n,data in enumerate(data):
+            self.set(x,n,data)
+    def __iter__(self):
+        return iter(self.data)
+  
+def main():
+    pass

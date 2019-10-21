4816e15859f906d5570da8796d2eadd341a7d879
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-21 09:16:09 -0700

    Update interactive_map.py

--- pygame_interactive/interactive_map.py
+++ pygame_interactive/interactive_map.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 tilesize = 128 # pixels per tile
 
 def tiletosurface(tile):
@@ -8,6 +9,7 @@ def maptosurface(sx,sy,ex,ey,oholmap):
     pass
 
 def main(windowsize,tilepipe,OHOLMap):
+    wt = math.floor(windowsize/tilesize)
     cx,cy,first = 0,0,True
     if OHOLMap.data != {}:
        for x in OHOLMap.data:

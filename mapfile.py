1498e2184dc14113c8aefcfd85030fc874d2b171
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-21 07:56:11 -0700

    Update mapfile.py

--- mapfile.py
+++ mapfile.py
@@ -4,6 +4,23 @@ def test_mapfile():
     data = fl.read().split("/n")
     fl.close()
     cache = {}
-    print(data)
+    for loc in data:
+        x,y,biome,floor,ontop = loc.split(" ")
+        if "," in ontop:
+            things = ontop.split(",")
+            print("container: {}".format(things.pop(0)))
+            for item in things:
+                if ";" in item:
+                    items = item.split(";")
+                    print("    container: {}".format(items.pop(0)))
+                    for citem in items:
+                        print("        has: {}".format(citem))
+                else:                  
+                    print("    has: {}".format(item))
+        else:
+            print("item:{}".format(ontop))
+       
+                  
+                  
     
     
